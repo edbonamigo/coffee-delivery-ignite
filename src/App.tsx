bae9277d56c4bb8e431105ff0c503b12a0bac7da
edbonamigo <edbmuller@gmail.com>
2023-08-27 20:47:36 -0300

    :sparkles: Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
+import { NavLink } from "react-router-dom"
+
 export function App() {
   
   return (
-    <>
+    <BrowserRouter>
       <h1>Coffe Delivery</h1>
-    </>
+      <NavLink to="/">
+        Home
+        </NavLink>
+      <NavLink to="/checkout">
+        Checkout
+      </NavLink>
+      <NavLink to="/Success">
+        Success
+      </NavLink>
+      <Router /> 
+    </BrowserRouter>
   )
 }
 
