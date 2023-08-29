5bc1fe2b1c73b57b300671a3675eb08fbcccad20
edbonamigo <edbmuller@gmail.com>
2023-08-29 14:01:51 -0300

    :lipstick: Add typography styles

---
+++ src/styles/themes/typography.ts
@@ -0,0 +1,85 @@
+import styled from 'styled-components'
+
+export const TitleXL = styled.h1`
+  font-family: 'Baloo 2', cursive;
+  font-size: 4.8rem;
+  line-height: 1.3;
+  font-weight: 800;
+`
+
+export const TitleL = styled.h2`
+  font-family: 'Baloo 2', cursive;
+  font-size: 3.2rem;
+  line-height: 1.3;
+  font-weight: 800;
+`
+
+export const TitleM = styled.h3`
+  font-family: 'Baloo 2', cursive;
+  font-size: 2.4rem;
+  line-height: 1.3;
+  font-weight: 800;
+`
+
+export const TitleS = styled.h4`
+  font-family: 'Baloo 2', cursive;
+  font-size: 2rem;
+  line-height: 1.3;
+  font-weight: 700;
+`
+
+export const TitleXS = styled.h5`
+  font-family: 'Baloo 2', cursive;
+  font-size: 1.8rem;
+  line-height: 1.3;
+  font-weight: 700;
+`
+
+export const TextL = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 2rem;
+  line-height: 1.3;
+  font-weight: 400;
+`
+ 
+export const TextM = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.6rem;
+  line-height: 1.3;
+  font-weight: 400;
+`
+ 
+export const TextS = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.4rem;
+  line-height: 1.3;
+  font-weight: 400;
+`
+ 
+export const TextXS = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.2rem;
+  line-height: 1.3;
+  font-weight: 700;
+`
+ 
+export const Tag = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  line-height: 1.3;
+  font-weight: 700;
+`
+ 
+export const ButtonG = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.4rem;
+  line-height: 1.6;
+  font-weight: 700;
+`
+ 
+export const ButtonM = styled.p`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.2rem;
+  line-height: 1.6;
+  font-weight: 400;
+`
